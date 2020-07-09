4ff5d786b76e66f5454c05e7fc06bd23b01fd9c9
Leah <8636027+Lmamaril@users.noreply.github.com>
2020-07-09 11:34:12 -0700

    optimize accident data load

--- etl.py
+++ etl.py
@@ -36,8 +36,29 @@ def process_road_rankings_data(cur):
     for index, row in df.iterrows():
         row_rank = (row.overall_rank, row.year, row.state, row.commute_time, row.public_transit, row.road_quality, row.bridge_quality)
         cur.execute(insert_road_rank, row_rank)
-    
+
 def process_accidents_data(cur):
+    df = pd.read_csv('./data/us_accidents.csv', nrows=100000)
+    ndf = df[['ID', 'Severity','Start_Time', 'End_Time', 'Start_Lat', 
+         'Start_Lng', 'Description','City', 'County', 'State',
+          'Weather_Timestamp', 'Temperature(F)', 
+          'Wind_Chill(F)', 'Humidity(%)', 
+          'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)',
+          'Precipitation(in)', 'Weather_Condition', 'Amenity', 
+          'Bump', 'Crossing', 'Give_Way', 'Junction', 
+          'No_Exit', 'Railway', 'Roundabout', 'Stop', 
+           'Traffic_Calming', 'Traffic_Signal',
+          'Turning_Loop','Sunrise_Sunset',
+            'Civil_Twilight', 'Nautical_Twilight', 'Astronomical_Twilight']]
+
+    tmp_file = 'file.csv'
+    ndf.to_csv(tmp_file, index=False, header=False)
+
+    file = open(tmp_file, 'r')
+    cur.copy_from(file, "accidents", sep=",", null="")
+
+    
+def process_time_data(cur):
     """ process the data for the accidents table and accidentsAnalysis table
     Args:
         ::cur:: psycopg2 cursor object
@@ -45,53 +66,11 @@ def process_accidents_data(cur):
         ::None::
     """
     
-    # read the accidents csv 1000 rows at a time
-    df = pd.read_csv('./data/us_accidents.csv', delimiter=',', chunksize=1000, nrows=1000)
+    # read the accidents csv 5000 row for a chunk at a time
+    df = pd.read_csv('./data/us_accidents.csv', delimiter=',', chunksize=5000, nrows=100000)
     for chunk in df:
-        for index, row in chunk.iterrows():
-            
-            # Handle null Weather_Timestamp
-            weather_ts = pd.to_datetime(row.Weather_Timestamp)
-            if pd.isnull(weather_ts):
-                weather_ts = None                
-            
-            accident_row = (row.ID, 
-                row.Severity, 
-                row.Start_Time, 
-                row.End_Time, 
-                row.Start_Lat, 
-                row.Start_Lng, 
-                row.Description, 
-                row.City, 
-                row.County, 
-                row.State,
-                weather_ts, 
-                row['Temperature(F)'], 
-                row['Wind_Chill(F)'], 
-                row['Humidity(%)'], 
-                row['Pressure(in)'],
-                row['Visibility(mi)'], 
-                row['Wind_Speed(mph)'],
-                row['Precipitation(in)'],
-                row.Weather_Condition, 
-                row.Amenity, 
-                row.Bump, 
-                row.Crossing,
-                row.Give_Way, 
-                row.Junction, 
-                row.No_Exit, 
-                row.Railway, 
-                row.Roundabout, 
-                row.Stop, 
-                row.Traffic_Calming, 
-                row.Traffic_Signal, 
-                row.Turning_Loop,
-                row.Sunrise_Sunset,
-                row.Civil_Twilight,
-                row.Nautical_Twilight,
-                row.Astronomical_Twilight)
-            cur.execute(insert_accidents,accident_row)
-            
+        for index, row in chunk.iterrows():            
+                                 
             def convert_to_time_row(timestamp):
                 """ converts 
                 Args:
@@ -107,7 +86,11 @@ def process_accidents_data(cur):
             dt_end = convert_to_time_row(row.End_Time)
             time_conversions = [dt_start, dt_end]
             
-            if weather_ts is not None:
+            # Handle null Weather_Timestamp
+            weather_ts = pd.to_datetime(row.Weather_Timestamp)
+            if pd.isnull(weather_ts):
+                weather_ts = None   
+            else:
                 time_conversions.append(convert_to_time_row(row.Weather_Timestamp)) 
             for time in time_conversions:
                 cur.execute(insert_time, time)
@@ -143,6 +126,7 @@ def main():
     process_holidays_data(cur)
     process_road_rankings_data(cur)
     process_accidents_data(cur)
+    process_time_data(cur)
     
     conn.close()
     time_delta = str(time.time() - start_time)
