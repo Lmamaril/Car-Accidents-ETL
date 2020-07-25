f92429dc3ded53075423ab016eeb07b1afe58336
Leah Mamaril <lmamaril@gmail.com>
2020-07-24 18:40:13 -0700

    added connect database

---
+++ db_modules/db_connection.py
@@ -0,0 +1,27 @@
+import psycopg2
+import config
+
+"""
+    This module is the accesspoint for db connection 
+    throughout the project
+"""
+
+def connect_database():
+    """ connect to the database
+    args:
+        :: None ::
+    returns:
+        :: conn :: psycopg2 connect object
+        :: cur :: psycopg2 cursor
+    """
+    conn =  psycopg2.connect(
+        "host={} dbname=postgres user={} password={}"
+        .format(hostname, username, pw))
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+    
+    return conn, cur
+
+def test_access():
+    return "access granted"
+
